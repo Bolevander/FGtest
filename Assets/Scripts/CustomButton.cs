4b59f054497df95a811a1cf21a751486f0ebb5f5
Bolevander <fayst0313@yandex.ru>
2023-12-06 14:53:24 +0300

    custom button added

---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animator),
+    typeof(ParticleSystem),
+    typeof(AudioSource))]
+public sealed class CustomButton : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private string _pressedAnimationName = "Pressed";
+    
+    private Animator _animator;
+    private ParticleSystem _particleSystem;
+    private AudioSource _audioSource;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlayAnimator();
+        PlayParticles();
+        PlayAudio();
+    }
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _particleSystem = GetComponent<ParticleSystem>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void PlayAnimator()
+    {
+        _animator.StopPlayback();
+        _animator.Play(_pressedAnimationName);
+    }
+    
+    private void PlayParticles()
+    {
+        _particleSystem.Stop();
+        _particleSystem.Clear();
+        _particleSystem.Play();
+    }
+    
+    private void PlayAudio()
+    {
+        _audioSource.Stop();
+        _audioSource.time = 5f;
+        _audioSource.Play();
+    }
+}
